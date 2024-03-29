2aea7139e6df9577deebb496726ff7331d5fef80
Brandon Lewallen <lewallenbrandon@gmail.com>
2024-03-28 19:27:16 -0500

    Remove the doom themes stuff.

--- elisp/bjl-theme.el
+++ elisp/bjl-theme.el
@@ -2,9 +2,6 @@
 
 (use-package all-the-icons)
 
-;;(use-package doom-themes
-;;  :init (load-theme 'doom-palenight t))
-
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
