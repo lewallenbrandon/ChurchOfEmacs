b2b7a5470b1ca02d2531adae4727a51932f26340
Brandon Lewallen <lewallenbrandon@gmail.com>
2024-03-28 19:11:22 -0500

    Theme changes. Making it prettier

--- elisp/bjl-theme.el
+++ elisp/bjl-theme.el
@@ -5,13 +5,21 @@
 ;;(use-package doom-themes
 ;;  :init (load-theme 'doom-palenight t))
 
-(add-to-list 'load-path "~/.emacs.d/modus-themes")
-(require 'modus-themes)
-(load-theme 'modus-vivendi-tinted :no-confirm)
-
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
+
+(add-to-list 'load-path "~/.emacs.d/modus-themes")
+(require 'modus-themes)
+
+(setq modus-vivendi-tinted-palette-overrides
+      '((bg-main "#1d2235")))
+
+(setq modus-themes-common-palette-overrides
+      '((border-mode-line-active bg-mode-line-active)
+        (border-mode-line-inactive bg-mode-line-inactive)))
+
+(load-theme 'modus-vivendi-tinted :no-confirm)
 
 
 (provide 'bjl-theme)
