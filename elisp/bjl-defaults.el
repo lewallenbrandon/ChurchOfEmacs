6a93d8334c819fb2a2908e70f2775fd3280a1426
Brandon Lewallen <lewallenbrandon@gmail.com>
2024-03-30 21:01:21 -0500

    Refactoring and moving things around

--- elisp/bjl-defaults.el
+++ elisp/bjl-defaults.el
@@ -1,43 +1,29 @@
 ;; Default Settings
 
-(defvar bjl/default-font-size 140)
-(defvar bjl/default-variable-font-size 140)
-(defvar bjl/frame-transparency '(90 . 90))
-(display-time-mode +1)
-
-;;(add-hook 'org-mode-hook #'my-org-faces)
-;; no-littering doesn't set this by default so we must place
 ;; auto save files in the same path as it uses for sessions
 (setq auto-save-file-name-transforms
       `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+(setq auto-save-file-name-transforms '((".*" "~/.emacs-saves/" t)))
+(setq backup-directory-alist '(("." . "~/.emacs-backups/")))
+(setq initial-major-mode 'org-mode)
 
+;; No start up message
 (setq inhibit-startup-message t)
 
+;; Hodge podge of settings
 (setq display-line-numbers-type 'relative) ; Relatively and Absolutely Correct way to Display Line Numbers
 (global-display-line-numbers-mode 1) ; Display the Line Numbers
+(column-number-mode)        ; Display a column number 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)          ; Disable the menu bar
+(setq visible-bell nil)     ; Disable visible bell
+(display-time-mode +1)      ; Time 
 
-(menu-bar-mode -1)            ; Disable the menu bar
 
-;; Set up the visible bell
-(setq visible-bell nil)
-
-(column-number-mode)
-
-;; Set frame transparency
-;;(set-frame-parameter (selected-frame) 'alpha bjl/frame-transparency)
-;;(add-to-list 'default-frame-alist `(alpha . ,bjl/frame-transparency))
-(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(set-face-attribute 'default nil :font "Jetbrains Mono" :height bjl/default-font-size)
-
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Jetbrains Mono" :height bjl/default-font-size)
-
+;; If I specify a path and it doesn't exist, well, make it exist.
 (defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
   "Create parent directory if not exists while visiting file."
   (unless (file-exists-p filename)
@@ -45,13 +31,6 @@
       (unless (file-exists-p dir)
         (make-directory dir t)))))
 
-
-(setq auto-save-file-name-transforms '((".*" "~/.emacs-saves/" t)))
-(setq backup-directory-alist '(("." . "~/.emacs-backups/")))
-(setq initial-major-mode 'org-mode)
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package eterm-256color
   :hook (term-mode . eterm-256color-mode))
--- elisp/bjl-theme.el
+++ elisp/bjl-theme.el
@@ -1,5 +1,15 @@
 ;; Theme Options
 
+(defvar custom-font-size 140)
+(defvar custom-variable-font-size 140)
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(set-face-attribute 'default nil :font "Jetbrains Mono" :height custom-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Jetbrains Mono" :height custom-font-size)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 (use-package all-the-icons)
 
 (use-package doom-modeline
@@ -17,6 +27,5 @@
         (border-mode-line-inactive bg-mode-line-inactive)))
 
 (load-theme 'modus-vivendi-tinted :no-confirm)
-
 
 (provide 'bjl-theme)
